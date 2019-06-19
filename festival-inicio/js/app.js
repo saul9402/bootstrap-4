ca4fe97a461c4d4475981e7f09e7ab87f4b2e0e0
Saul Avila <saul_harry_@hotmail.com>
2019-06-19 15:21:12 -0500

    se agrega barra de navegación con estilos y la clase fixed-top para hacer que se quede fija durante toda la navegacion

--- festival-inicio/js/app.js
+++ festival-inicio/js/app.js
@@ -1,13 +1,25 @@
 
+// cambio de color el fondo al hacer scroll
+
+window.onscroll = (e) => {
+    const scroll = window.scrollY;
+    const header = document.querySelector( '#navegacion-principal');
+    if(scroll > 300){
+        header.classList.add('bg-success');
+    }else{
+        header.classList.remove('bg-success');
+    }
+}
 
 
-
-
-
-
+// countdown
 
 $(document).ready(function () {
     $('#fecha').countdown('2019/08/23', function (evento) {
-        $(this).html(evento.strftime('<span> %D </span> Dias <span> %H </span> Horas <span> %M </span> Minutos <span> %S </span> Segundos'));
+        $(this).html(evento.strftime(`
+            <span> %D </span> Dias
+            <span> %H </span> Horas
+            <span> %M </span> Minutos
+            <span> %S </span> Segundos`));
     });
 });
