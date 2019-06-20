f8f2e42a86f4032102089af04a8c23f1f5a7e81e
Saul Avila <saul_harry_@hotmail.com>
2019-06-20 16:27:24 -0500

    se termina de agregar estilos al menu y el efecto de que aparezca y desaparezca según que tanto scroll le hayas dado a la pagina

---
+++ fiverr-inicio/js/app.js
@@ -0,0 +1,11 @@
+window.onscroll = () => {
+    let scroll = window.scrollY;
+    
+    const headerScroll = document.querySelector('.header-scroll');
+
+    if(scroll > 300){
+        headerScroll.classList.add('activo');
+    }else{
+        headerScroll.classList.remove('activo');
+    }
+}
