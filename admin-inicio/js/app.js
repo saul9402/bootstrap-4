548bfe77ed8a4df1cba309d93ea0eb44bb1bf37a
Saul Avila <saul_harry_@hotmail.com>
2019-06-21 10:16:15 -0500

    se agrega morris para mostrar las graficas de las ventas

--- admin-inicio/js/app.js
+++ admin-inicio/js/app.js
@@ -16,3 +16,33 @@
       });
     }, false);
   })();
+  new Morris.Line({
+    // ID of the element in which to draw the chart.
+    element: 'ventas',
+    // Chart data records -- each entry in this array corresponds to a point on
+    // the chart.
+    data: [
+      { year: '2008', value: 20 },
+      { year: '2009', value: 10 },
+      { year: '2010', value: 5 },
+      { year: '2011', value: 5 },
+      { year: '2012', value: 20 },
+      { year: '2013', value: 20 },
+      { year: '2014', value: 10 },
+      { year: '2015', value: 5 },
+      { year: '2016', value: 5 },
+      { year: '2017', value: 20 },
+      { year: '2018', value: 20 },
+      { year: '2019', value: 10 },
+      { year: '2020', value: 5 },
+      { year: '2021', value: 5 },
+      { year: '2022', value: 20 }
+    ],
+    // The name of the data record attribute that contains x-values.
+    xkey: 'year',
+    // A list of names of data record attributes that contain y-values.
+    ykeys: ['value'],
+    // Labels for the ykeys -- will be displayed when you hover over the
+    // chart.
+    labels: ['Value']
+  });
